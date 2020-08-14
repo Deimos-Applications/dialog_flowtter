557ffbb387071fdbbdabbc41d859991ab247d0d3
Israel <dragon-zisrael@hotmail.com>
2020-08-14 07:00:42 -0500

    Exported code and commented tests (for now)

--- lib/dialog_flowtter.dart
+++ lib/dialog_flowtter.dart
@@ -1,7 +1,12 @@
 library dialog_flowtter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './src/auth.dart';
+export './src/dialog.dart';
+export './src/models/detect_intent_response.dart';
+export './src/models/follow_up_intent_info.dart';
+export './src/models/intent.dart';
+export './src/models/message.dart';
+export './src/models/output_audio_config.dart';
+export './src/models/output_contexts.dart';
+export './src/models/query_result.dart';
+export './src/models/text.dart';
