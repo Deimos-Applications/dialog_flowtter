f66df9d0cfe17f4188f9652003b9347124a3f135
nadia-hilmi <71830409+nadia-hilmi@users.noreply.github.com>
2022-12-05 10:25:18 +0100

    Update sentiment.g.dart
    Cast error fixed: if score is "null" it cannot be cast as a "num" type

--- lib/src/models/detect_intent_response/sentiment.g.dart
+++ lib/src/models/detect_intent_response/sentiment.g.dart
@@ -7,8 +7,8 @@ part of 'sentiment.dart';
 // **************************************************************************
 
 Sentiment _$SentimentFromJson(Map<String, dynamic> json) => Sentiment(
-      score: (json['score'] as num).toDouble(),
-      magnitude: (json['magnitude'] as num).toDouble(),
+      score: (json['score'] == null) ? 0 : (json['score'] as num).toDouble(),
+      magnitude: (json['magnitude'] == null)? 0: (json['magnitude'] as num).toDouble(),
     );
 
 Map<String, dynamic> _$SentimentToJson(Sentiment instance) => <String, dynamic>{
